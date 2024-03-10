a9654d907f8d7a776b815bcc874a15f8287f6321
singhania-sa <147664492+singhania-sa@users.noreply.github.com>
2024-03-10 16:31:12 -0400

    added router from main page to form page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,32 @@
+'use client'
 import Image from "next/image";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const navigation = useRouter();
+
+  const handleSubmission = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    navigation.push('app/matching-form');
+  };
+
   return (
     <main>
       <h1 className="text-3xl font-bold">Fundies Partner Match</h1>
     
-      <form action="" method="get" className="form">
-  <div className="form">
-    <label>Username: </label>
-    <input type="text" name="name" id="name" required />
-  </div>
-  <div className="form">
-    <label>Password: </label>
-    <input type="password" name="password" id="password" required />
-  </div>
-  <div className="form">
-    <input type="submit" value="Log In" />
-  </div>
-</form>
+  <form onSubmit={handleSubmission} className="form">
+    <div className="form">
+      <label>Username: </label>
+      <input type="text" name="name" id="name" required />
+    </div>
+    <div className="form">
+      <label>Password: </label>
+      <input type="password" name="password" id="password" required />
+    </div>
+    <div className="form">
+      <input type="submit" value="Log In" />
+    </div>
+  </form>
     
     
     </main>
