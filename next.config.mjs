71a4c42e62ae1000e1bdf99547b4623f349a18bd
singhania-sa <147664492+singhania-sa@users.noreply.github.com>
2024-03-16 03:31:45 -0400

    updates nextConfig for flask

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:5328/:path*',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
 
-export default nextConfig;
+// export default {
+//     async rewrites() {
+//       return [
+//         {
+//           source: '/api/:path*',
+//           destination: 'http://127.0.0.1:5328/:path*', // Proxy to Backend
+//         },
+//       ];
+//     },
+//   };
+  
